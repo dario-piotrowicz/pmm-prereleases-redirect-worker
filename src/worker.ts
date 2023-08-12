9070f37efce5b6b2a393953371262c8b22407798
dario-piotrowicz <dario.piotrowicz@gmail.com>
2023-08-12 16:13:37 +0100

    apply redirect logic

--- src/worker.ts
+++ src/worker.ts
@@ -1,32 +1,14 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
-export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
-}
-
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+		const paths = new URL(request.url).pathname.split('/');
+		if (paths.length !== 3) {
+			return new Response('Wrong request', { status: 400 });
+		}
+		const workflowId = paths[1];
+		const workflowPR = paths[2];
+		return Response.redirect(
+			`https://prerelease-registry.ixion-labs.workers.dev/james-elicx/package-manager-manager/runs/${workflowId}/npm-package-package-manager-manager-${workflowPR}`,
+			301
+		);
 	},
 };
